00ec1b9c74cfd0ea916aff03b91fa4658d6663a4
DANILO DA CRUZ MUNHE <21276321@CFP127ALUNO.LOCAL>
2021-11-16 13:39:29 -0300

    GameRepository finalizado

--- src/br/senai/sp/jandira/repository/GamesRepository.java
+++ src/br/senai/sp/jandira/repository/GamesRepository.java
@@ -5,4 +5,21 @@ import br.senai.sp.jandira.model.Jogo;
 public class GamesRepository {
 
 	private Jogo[] colecaoGames;
+
+	public GamesRepository() {
+		colecaoGames = new Jogo[32];
+	}
+
+	public GamesRepository(int quatidadeDeJogos) {
+		colecaoGames = new Jogo[quatidadeDeJogos];
+	}
+	public void gravarJogo(Jogo jogo, int posicao) {
+		colecaoGames = new Jogo[posicao];
+	}
+	public Jogo listarJogos(int posicao) {
+		return colecaoGames[posicao];
+	}
+	public Jogo[] listatTodosJogos() {
+		return colecaoGames;
+	}
 }
